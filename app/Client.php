056231658a2f2220ba131c2cab23a6e6970adcb3
sombodi <perrodeagua@gmail.com>
2017-03-09 13:18:34 -0600

    client model

--- app/Client.php
+++ app/Client.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'email', 'telephone', 'payload'
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'payload' => 'array',
+    ];
+
+    
 }
