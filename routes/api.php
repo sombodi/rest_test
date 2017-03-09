5b0f3bb546bb24d23491418fd7215667adbd4da3
sombodi <perrodeagua@gmail.com>
2017-03-09 14:37:32 -0600

    Fix middleware usage and use resource instead

--- routes/api.php
+++ routes/api.php
@@ -13,14 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::group(['middleware' => 'auth.api'], function () {
-    Route::get('/clients', 'ClientController@index');
-    Route::get('/clients/{id}', 'ClientController@show');
-    Route::post('/clients', 'ClientController@store');
-    Route::put('/clients/{id}', 'ClientController@update');
-    Route::delete('/clients/{id}', 'ClientController@destroy');
+Route::group(['prefix' => 'v1'], function () { 
+    Route::resource('/clients', 'ClientController');
 });
